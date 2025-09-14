28357785c7b5a487529d246e139d90e4b55133a6
mauro <mauroricceli@gmail.com>
2025-09-13 22:49:34 -0300

    first commit

---
+++ src/main/java/com/practice2/practice2sboot/MainApplication.java
@@ -0,0 +1,13 @@
+package com.practice2.practice2sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
